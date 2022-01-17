46c2d6b731b4d4ce7f859d01feb746bf35e7205f
MyNameTsThad <pvpthadgaming@gmail.com>
2022-01-17 10:01:23 +0700

    sudoers role failsafe

--- src/main/java/io/github/mynametsthad/somediscordbot/features/Commands.java
+++ src/main/java/io/github/mynametsthad/somediscordbot/features/Commands.java
@@ -122,12 +122,34 @@ public class Commands extends ListenerAdapter {
                                             Initializing guild...
                                             [:white_check_mark:] Created `sudoers` role
                                             [ ] Adding `sudoers` role to requested user...
-                                            [ ] Adding server-specific configurations...""").queue();
+                                            [ ] Adding server-specific configurations...""")
+                                            .queue();
+                                    event.getGuild().addRoleToMember(Objects.requireNonNull(event.getMember()), Objects.requireNonNull(event.getGuild().getRoleById(SomeDiscordBot.instance.configs.sudoersRankIDs.get(event.getGuild().getId()))))
+                                            .queue(nextstep -> response.editMessage("""
+                                                                Initializing guild...
+                                                                [:white_check_mark:] Created `sudoers` role
+                                                                [:white_check_mark:] Added `sudoers` role to requested user
+                                                                [ ] Adding server-specific configurations...""").queue(nextstep2 -> {
+                                                if (!SomeDiscordBot.instance.configs.prefixes.containsKey(event.getGuild().getId())) {
+                                                    SomeDiscordBot.instance.configs.prefixes.put(event.getGuild().getId(), "sdb|");
+                                                    SomeDiscordBot.instance.configs.journalChannels.put(event.getGuild().getId(), "");
+                                                    try {
+                                                        SomeDiscordBot.instance.configs.saveToFile();
+                                                    } catch (IOException e) {
+                                                        e.printStackTrace();
+                                                    }
+                                                }
+                                                response.editMessage("""
+                                                                    Guild initialized.
+                                                                    [:white_check_mark:] Created `sudoers` role
+                                                                    [:white_check_mark:] Added `sudoers` role to requested user
+                                                                    [:white_check_mark:] Added server-specific configurations""").queue();
+                                            }));
                                 }
                             });
                 } else {
                     event.getMessage().reply("""
-                            Failed to initialize guild.
+                            Initializing guild...
                             [:x:] Failed to create `sudoers` role - Role already exists! Please delete the role and try again.
                             [ ] Adding `sudoers` role to requested user...
                             [ ] Adding server-specific configurations...""").queue();
