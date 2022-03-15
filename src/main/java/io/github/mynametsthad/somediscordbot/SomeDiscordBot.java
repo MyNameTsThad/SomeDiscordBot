8debc1a8bc13284a30c0eaafaf7819d247189416
MyNameTsThad <pvpthadgaming@gmail.com>
2022-03-15 16:06:12 +0700

    update version id

--- src/main/java/io/github/mynametsthad/somediscordbot/SomeDiscordBot.java
+++ src/main/java/io/github/mynametsthad/somediscordbot/SomeDiscordBot.java
@@ -19,8 +19,8 @@ import javax.security.auth.login.LoginException;
 public class SomeDiscordBot {
     public static final String NAME = "Some Discord Bot";
     public static final String SHORTNAME = "SomeDiscordBot";
-    public static final String VERSION = "1.1.0-1";
-    public static final int VERSION_ID = 5;
+    public static final String VERSION = "1.1.0-2";
+    public static final int VERSION_ID = 64;
     public static final String TOKEN = ""; //token here
 
     public static final boolean devMode = false;
