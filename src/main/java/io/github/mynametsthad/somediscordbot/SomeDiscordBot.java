b77dfc4940b35b2e10de56f6b1246470fa797fdd
IWant2Tryhard <62128640+MyNameTsThad@users.noreply.github.com>
2022-03-16 13:43:10 +0700

    make bot not mistake parts of words as trigger words

--- src/main/java/io/github/mynametsthad/somediscordbot/SomeDiscordBot.java
+++ src/main/java/io/github/mynametsthad/somediscordbot/SomeDiscordBot.java
@@ -19,8 +19,8 @@ import javax.security.auth.login.LoginException;
 public class SomeDiscordBot {
     public static final String NAME = "Some Discord Bot";
     public static final String SHORTNAME = "SomeDiscordBot";
-    public static final String VERSION = "1.2.1";
-    public static final int VERSION_ID = 67;
+    public static final String VERSION = "1.2.1-1";
+    public static final int VERSION_ID = 68;
     public static final String TOKEN = ""; //token here
 
     public static final boolean devMode = false;
